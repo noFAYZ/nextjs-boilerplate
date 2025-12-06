2e5f4d8856c931b8b8f3b37aabd066b50a8a2d78
Faizan Asad <p176056@nu.edu.pk>
2025-12-06 13:03:46 +0500

    as

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,10 +4,8 @@ export function initPostHog() {
   if (typeof window !== "undefined") {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "/ingest",
-      ui_host: "https://us.posthog.com",
-      capture_exceptions: true,
-      debug: process.env.NODE_ENV === "development",
-      enabled: true,
+     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      defaults: '2025-11-30'
     })
   }
 }
